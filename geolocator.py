c8e17c9b68ef258125319cf52a6794a1afc6c344
TheRogue <36058852+rogue7373@users.noreply.github.com>
2022-01-24 18:32:42 -0700

    Bug fixes

--- geolocator.py
+++ geolocator.py
@@ -1,25 +1,29 @@
 #!/usr/bin/python3
 
+# import system libraries
 import sys
 import requests
 import socket
 import json
 
-if len(sys.argv) > 2:
+# error control
+if len(sys.argv) < 2:
     print("Usage: " + sys.argv[0] + "<url>")
     sys.exit(1)
 
-req = requests.get("https://"sys.argv[1])
-print("\n" +str(req.headers))
+# URL input from user for get request and banner grabbing
+req = requests.get("https://"+sys.argv[1])
+print("\n"+str(req.headers))
 
-gethostby = socket.gethostbyaddr(sys.argv[1])
-print("\nThe IP address of " + sys.argv[1] + " is " + gethostby + "\n")
+# Print statement to display the IP address information
+gethostby_ = socket.gethostbyname(sys.argv[1])
+print("\nThe IP address of "+sys.argv[1]+" is: "+gethostby_ + "\n")
 
-#ipinfo.io API call 
-
-req_two = requests.get("https:/ipinfo.io/" +gethostby_+"/json")
+#ipinfo.io API call to get the location information
+req_two = requests.get("https://ipinfo.io/" +gethostby_+"/json")
 resp_ = json.loads(req_two.text)
 
+# Print statement to display the location information
 print("Location: "+resp_["loc"])
 print("Region: "+resp_["region"])
 print("City: "+resp_["city"])
